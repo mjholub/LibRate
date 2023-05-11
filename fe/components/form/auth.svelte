3684fa95694cc8c55b95ae3df2ac32146da0d8fa
MJH <1a6f1a@riseup.net>
2023-05-12 01:23:22 +0200

    chore: bootstrap frontend, add auth form

---
+++ fe/components/form/auth.svelte
@@ -0,0 +1,75 @@
+<script>
+  import { onMount } from "svelte";
+
+  let email_or_username = "";
+  let password = "";
+  let confirmPassword = "";
+  let passwordStrength = "";
+  let errorMessage = "";
+
+  const checkEntropy = async () => {
+    const response = await fetch(`/api/password-entropy?password=${password}`);
+    const data = await response.json();
+    passwordStrength = data.entropy;
+  };
+
+  const register = async () => {
+    if (password !== confirmPassword) {
+      errorMessage = "Passwords do not match";
+      return;
+    }
+
+    if (passwordStrength < 60) {
+      errorMessage = "Password is not strong enough";
+      return;
+    }
+
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      // auto login the new user and render a confirmation prompt top bar
+      const { token } = await response.json();
+      if (token) {
+        localStorage.setItem("token", token);
+        window.location = "/";
+      }
+    } else {
+      const { message } = await response.json();
+      errorMessage = message;
+    }
+    // end of token validation branch
+  };
+  // end of response.ok branch
+
+  $: password && checkEntropy();
+</script>
+
+<form on:submit|preventDefault={register}>
+  <label for="email">Email or username:</label>
+  <input id="email_or_username" bind:value={email_or_username} required />
+
+  <label for="password">Password:</label>
+  <input id="password" bind:value={password} type="password" required />
+
+  <label for="confirmPassword">Confirm Password:</label>
+  <input
+    id="confirmPassword"
+    bind:value={confirmPassword}
+    type="password"
+    required
+  />
+
+  <p>Password strength: {passwordStrength} bits</p>
+
+  {#if errorMessage}
+    <p style="color: red;">{errorMessage}</p>
+  {/if}
+
+  <button type="submit">Sign Up</button>
+</form>
