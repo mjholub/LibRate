a53a87704c4fe015c38e5991f648333b2a51cffb
MJH <1a6f1a@riseup.net>
2023-05-22 16:34:20 +0200

    refactor: delegte routes setup

--- main.go
+++ main.go
@@ -1,30 +1,19 @@
 package main
 
 import (
-	"net/http"
-	"path/filepath"
-
 	"codeberg.org/mjh/LibRate/cfg"
-	"codeberg.org/mjh/LibRate/utils"
+	"codeberg.org/mjh/LibRate/routes"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
-	log := utils.NewLogger()
-	log.Info("Starting server...")
 	app := fiber.New()
+	app.Use(logger.New())
 	cfg.LoadConfig()
 
-	staticPath, err := filepath.Abs("./fe/public")
-	if err != nil {
-		log.Sugar().Fatalf("Error loading static path: %s", err)
-	}
+	routes.Setup(app)
 
-	app.Use("/", filesystem.New(filesystem.Config{
-		Root:   http.Dir(staticPath),
-		Browse: true,
-	}))
 	app.Listen(":3000")
 }
---
+++ routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"net/http"
+	"path/filepath"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+
+	"codeberg.org/mjh/LibRate/controllers"
+	"codeberg.org/mjh/LibRate/controllers/auth"
+	"codeberg.org/mjh/LibRate/utils"
+)
+
+func Setup(app *fiber.App) {
+	log := utils.NewLogger()
+	staticPath, err := filepath.Abs("./fe/public")
+	if err != nil {
+		log.Sugar().Fatalf("Error loading static path: %s", err)
+	}
+
+	app.Use("/", filesystem.New(filesystem.Config{
+		Root:   http.Dir(staticPath),
+		Browse: true,
+	}))
+
+	app.Get("/api/reviews/:id", controllers.GetReviews)
+	app.Post("/api/reviews", controllers.PostReview)
+	app.Post("/api/login", auth.Login)
+	app.Post("/api/register", auth.Register)
+}
