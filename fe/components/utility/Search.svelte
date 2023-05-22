9d092456b9bdd7b99cf75743e4590f437f648e01
MJH <1a6f1a@riseup.net>
2023-05-22 15:41:12 +0200

    fix(FE): search handler

--- fe/components/utility/Search.svelte
+++ fe/components/utility/Search.svelte
@@ -1,23 +1,43 @@
 <script>
+  import { onMount } from "svelte";
+
   export let items = [];
 
   let search = "";
 
-  // TODO: add more versatile fuzzy finding in backend
-  const searchItems = () =>
-    items.filter((item) =>
-      item.name.toLowerCase().includes(search.toLowerCase())
-    );
+  // function to fetch data from the backend based on the search term
+  async function searchItems() {
+    const response = await fetch("http://localhost:3000/api/search", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ search }),
+    });
+
+    const data = await response.json();
+
+    // If the backend responds with the filtered items
+    // update items with the new data
+    items = data;
+  }
+
+  onMount(() => {
+    searchItems();
+  });
 </script>
 
 <div class="search-bar">
   <input
     type="text"
     bind:value={search}
-    placeholder="Search..."
-    on:input={() => {}}
+    placeholder="Enter search keywords..."
+    on:input={searchItems}
   />
+  <button on:click={searchItems}>Search</button>
+</div>
 
+<div>
   <ul class="search-results">
     {#each searchItems() as item (item.id)}
       <li class="search-result">{item.name}</li>
--- fe/src/App.svelte
+++ fe/src/App.svelte
@@ -1,7 +1,7 @@
 <script>
-  import ReviewList from "./components/review/reviewList.svelte";
-  import Auth from "./components/form/Auth.svelte";
-  import Search from "./components/utility/Search.svelte";
+  import ReviewList from "../components/review/ReviewList.svelte";
+  import Auth from "../components/form/Auth.svelte";
+  import Search from "../components/utility/Search.svelte";
   import { onMount } from "svelte";
 
   let reviews = [];
