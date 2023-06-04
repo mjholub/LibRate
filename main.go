b7fb8bcb5799ff8e516779a5a965277e7452ef54
MJH <1a6f1a@riseup.net>
2023-06-04 16:32:15 +0200

    fix(config): initialization in main

--- main.go
+++ main.go
@@ -6,17 +6,30 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+
+	"codeberg.org/mjh/LibRate/internal/logging"
 )
 
 func main() {
 	app := fiber.New()
 	app.Use(logger.New())
-	cfg.LoadConfig()
+	conf, err := cfg.LoadConfig()
+	log := logging.Init()
+	if err != nil {
+		log.Panic().Err(err).Msg("Failed to load config")
+	}
+	// CORS
+	app.Use("/api", func(c *fiber.Ctx) error {
+		c.Set("Access-Control-Allow-Origin", conf.Host)
+		c.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		return c.Next()
+	})
 
 	routes.Setup(app)
 
-	err := app.Listen(":3000")
+	err = app.Listen(":3000")
 	if err != nil {
-		panic(err)
+		log.Panic().Err(err).Msg("Failed to listen on port 3000")
 	}
 }
