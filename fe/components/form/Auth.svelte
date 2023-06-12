fa90af6648ff4980805ed3ae35e49ce53bed9139
MJH <1a6f1a@riseup.net>
2023-06-12 02:50:11 +0200

    feat(auth): toggling password visibility

--- fe/components/form/Auth.svelte
+++ fe/components/form/Auth.svelte
@@ -9,6 +9,10 @@
   let passwordStrength = 0;
   let errorMessage = "";
 
+  const toggleObfuscation = () => {
+    showPassword = !showPassword;
+  };
+
   const checkEntropy = async (password) => {
     const response = await fetch(`/api/password-entropy`, {
       method: "POST",
@@ -90,30 +94,38 @@
   <label for="email_or_username">Email or username:</label>
   <input id="email_or_username" bind:value={email_or_username} required />
 
-  <label for="password">Password:</label>
-  <input
-    id="password"
-    bind:value={password}
-    type="password"
-    required
-    class={!showPassword ? "" : "hidden"}
-  />
+  <div class="password-container">
+    <label for="password">Password:</label>
 
-  <input
-    id="textPassword"
-    bind:value={password}
-    type="text"
-    required
-    class={showPassword ? "" : "hidden"}
-  />
+    <input
+      id="password"
+      class={!showPassword ? "" : "hidden"}
+      bind:value={password}
+      type="password"
+      autocomplete="new-password"
+      required
+    />
 
-  <button
-    class="show-password"
-    type="button"
-    on:click={() => (showPassword = !showPassword)}
-  >
-    {showPassword ? "Hide" : "Show"} password
-  </button>
+    <!-- use the autocomplete property to prevent the browser from filling in the password -->
+    <input
+      id="textPassword"
+      class={showPassword ? "" : "hidden"}
+      bind:value={password}
+      type="text"
+      autocomplete="new-password"
+      required
+    />
+
+    <button
+      class="toggle-btn"
+      type="button"
+      on:click|preventDefault={toggleObfuscation}
+    >
+      <span class="material-icons">
+        {showPassword ? "visibility_off" : "visibility"}
+      </span>
+    </button>
+  </div>
 
   {#if isRegistration}
     <label for="confirmPassword">Confirm Password:</label>
@@ -156,9 +168,40 @@
     border-radius: 4px;
   }
 
-  .show-password {
-    margin-left: 0.5em;
+  .password-container {
     position: relative;
+    display: inline-block;
+  }
+
+  .hidden {
+    display: none;
+  }
+
+  .toggle-btn {
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    background: transparent;
     border: none;
+    cursor: pointer;
+  }
+
+  .material-icons {
+    font-family: "Material Icons";
+    font-weight: normal;
+    font-style: normal;
+    font-size: 20px; /* Preferred icon size */
+    display: inline-block;
+    line-height: 1;
+    text-transform: none;
+    letter-spacing: normal;
+    word-wrap: normal;
+    white-space: nowrap;
+    direction: ltr;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+    -moz-osx-font-smoothing: grayscale;
+    font-feature-settings: "liga";
   }
 </style>
