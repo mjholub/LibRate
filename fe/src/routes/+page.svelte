e5c860b70fd5de01bf154d730c678fa9cc76d1f9
MJH <1a6f1a@riseup.net>
2023-07-08 05:58:00 +0200

    chore(fe): use static adapter

---
+++ fe/src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import ReviewList from '../components/review/ReviewList.svelte';
+	import Auth from '../components/form/Auth.svelte';
+	import Search from '../components/utility/Search.svelte';
+	import type { Review } from '../types/review.ts';
+	import { onMount } from 'svelte';
+
+	let reviews: Review[] = [];
+</script>
+
+<div class="navbar">
+	<Search />
+</div>
+
+<div class="app">
+	<div class="left">
+		<ReviewList {reviews} />
+	</div>
+	<div class="right">
+		<Auth />
+	</div>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		justify-content: space-between;
+		background-color: #333;
+		color: #fff;
+		padding-top: 40px;
+	}
+
+	.left,
+	.right {
+		padding-top: 45px;
+		width: 30%;
+	}
+
+	.navbar {
+		position: fixed;
+		top: 0;
+		width: 100%;
+		background-color: #333;
+		color: #fff;
+		padding: 10px 0;
+		box-shadow: 0 2px 4px rgba(64, 64, 64, 0.2);
+		z-index: 1000;
+	}
+
+	:global(body) {
+		background-color: #333;
+		color: #fff;
+	}
+</style>
