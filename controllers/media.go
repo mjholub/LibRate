5e5758e9d7d4dcd7082fe4c37306fc0ae459ebdf
MJH <1a6f1a@riseup.net>
2023-08-24 19:47:17 +0200

    refactor(media): split controllers into smaller files

--- controllers/media.go
+++ controllers/media.go
@@ -3,17 +3,13 @@ package controllers
 import (
 	"context"
 	"database/sql"
-	"strconv"
-	"sync"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofrs/uuid/v5"
 
-	"codeberg.org/mjh/LibRate/internal/client"
 	h "codeberg.org/mjh/LibRate/internal/handlers"
 	"codeberg.org/mjh/LibRate/models"
-	services "codeberg.org/mjh/LibRate/recommendation/go/services"
 )
 
 type (
@@ -73,62 +69,6 @@ func (mc *MediaController) GetMedia(c *fiber.Ctx) error {
 	return h.ResData(c, fiber.StatusOK, "success", detailedMedia)
 }
 
-// GetRandom fetches up to 5 random media items to be displayed in a carousel on the home page
-func (mc *MediaController) GetRandom(c *fiber.Ctx) error {
-	mc.storage.Log.Info().Msg("Hit endpoint " + c.Path())
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	// FIXME: blacklist tracks, because they get e.g. improperly parsed as albums at some point
-	media, err := mc.storage.GetRandom(ctx, 2, "track")
-	if err != nil {
-		mc.storage.Log.Error().Err(err).Msgf("Failed to get random media: %s", err.Error())
-		return h.Res(c, fiber.StatusInternalServerError,
-			"Failed to get random media: "+err.Error())
-	}
-
-	mc.storage.Log.Info().Msgf("Got %d random media items", len(media))
-	mediaItems := make([]interface{}, len(media))
-
-	errChan := make(chan mediaError, len(media))
-	var wg sync.WaitGroup
-
-	// set an iterator variable so we can access the media item in the goroutine
-	i := 0
-	mc.storage.Log.Info().Msg("Getting media details")
-	for id, kind := range media {
-		wg.Add(1)
-		go func(i int, id uuid.UUID, kind string) {
-			defer wg.Done()
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			defer cancel()
-			// NOTE:this val. of err must be shadowed, otherwise it will be the same err for all goroutines
-			mDetails, err := mc.storage.
-				GetMediaDetails(ctx, kind, id)
-			if err != nil {
-				errChan <- mediaError{ID: id, Err: err}
-				return
-			}
-			mc.storage.Log.Info().Msgf("Got media details for media with ID %s", id.String())
-			mediaItems[i] = mDetails
-		}(i, id, kind)
-		i++
-	}
-
-	wg.Wait()
-	close(errChan)
-
-	if len(errChan) > 0 {
-		for e := range errChan {
-			mc.storage.Log.Error().Err(err).
-				Msgf("Failed to get media details for media with ID %s: %s", e.ID, e.Err.Error())
-		}
-		return h.Res(c, fiber.StatusInternalServerError, "Failed to get media details")
-	}
-
-	return h.ResData(c, fiber.StatusOK, "success", mediaItems)
-}
-
 func (mc *MediaController) GetImagePaths(c *fiber.Ctx) error {
 	mc.storage.Log.Info().Msg("Hit endpoint " + c.Path())
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -185,35 +125,6 @@ func (mc *MediaController) GetImagePaths(c *fiber.Ctx) error {
 }
 
 // WARN: this is probably wrong
-// GetRecommendations returns media recommendations for a user based on collaborative filtering
-// TODO: the actual underlying functionality, i.e. the recommendations server
-func GetRecommendations(c *fiber.Ctx) error {
-	mID, err := strconv.ParseInt(c.Params("id"), 10, 64)
-	if err != nil {
-		//nolint:errcheck
-		return h.Res(c, fiber.StatusBadRequest, fmt.Sprintf("Invalid member ID %s (must be an integer)", c.Params("id")))
-	}
-
-	memberID := int32(mID)
-
-	conn, err := client.ConnectToService(context.Background(), "recommendation", "50051")
-	if err != nil {
-		return h.Res(c, fiber.StatusInternalServerError, "Failed to connect to recommendation service")
-	}
-	defer conn.Close()
-
-	s := services.NewRecommendationServiceClient(conn)
-
-	recommendedMedia, err := s.GetRecommendations(context.Background(), &services.GetRecommendationsRequest{
-		MemberId: memberID,
-	})
-	if err != nil {
-		return h.Res(c, fiber.StatusInternalServerError, "Failed to get recommendations")
-	}
-
-	return c.JSON(recommendedMedia)
-}
-
 func AddGenre() {
 	// TODO: implement
 }
