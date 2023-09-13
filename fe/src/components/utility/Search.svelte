6cec323c4609e4c1424b624879f95e42dad3f148
MJH <1a6f1a@riseup.net>
2023-09-13 04:46:48 +0200

    fix(fe): error handling, conditional render in search

--- fe/src/components/utility/Search.svelte
+++ fe/src/components/utility/Search.svelte
@@ -1,25 +1,31 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import axios from 'axios';
 	import type { SearchItem } from '$lib/types/search.ts';
 
 	let search = '';
-	let items: SearchItem[] = [];
+	let items: SearchItem[] | null = null;
+	let error: string | null = null;
 
-	// function to fetch data from the backend based on the search term
 	async function searchItems() {
-		const response = await fetch('http://localhost:3000/api/search', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ search })
-		});
+		try {
+			const response = await axios.post(
+				'http://127.0.0.1:3000/api/search/',
+				{ search },
+				{
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+			);
 
-		const data = await response.json();
-
-		// If the backend responds with the filtered items
-		// update items with the new data
-		items = data;
+			items = response.data;
+			error = null;
+		} catch (err) {
+			console.error('Error fetching data:', err);
+			items = null;
+			error = 'An error occurred while fetching data.';
+		}
 	}
 
 	onMount(() => {
@@ -42,15 +48,37 @@
 	<button on:click={searchItems}>Search</button>
 </div>
 
-<div>
-	<ul class="search-results">
-		{#each items as item (item.id)}
-			<li class="search-result">{item.name}</li>
-		{/each}
-	</ul>
-</div>
+{#if error}
+	<p>{error}</p>
+{:else if items !== null}
+	{#if items.length === 0}
+		<p>No results found.</p>
+	{:else}
+		<div class="search-result-container">
+			<p>Found {items.length} result{items.length === 1 ? '' : 's'}.</p>
+			<ul class="search-results">
+				{#each items as item (item.id)}
+					<li class="search-result">{item.name}</li>
+				{/each}
+			</ul>
+		</div>
+	{/if}
+{:else}
+	<style>
+		.search-result-container {
+			display: none;
+		}
+	</style>
+{/if}
 
 <style>
+	.search-result-container {
+		display: flex;
+		flex-direction: column;
+		align-items: left;
+		padding-bottom: 0.5em;
+	}
+
 	.search-bar {
 		margin-bottom: 0.8em;
 		width: 50%; /* initial width */
@@ -64,6 +92,7 @@
 	.search-results {
 		list-style-type: none;
 		padding-left: 0;
+		padding-bottom: 0.5em;
 	}
 
 	.search-result {
