780fa1a27a0a1718eb03c7f89e04de27bed2d09e
MJH <1a6f1a@riseup.net>
2023-10-23 21:44:08 +0200

    feat(logging): load logging setup from config

--- internal/logging/config.go
+++ internal/logging/config.go
@@ -1,14 +1,14 @@
 package logging
 
 type Config struct {
-	Level     string          `yaml:"level"`
-	Target    string          `yaml:"target"`
-	Format    string          `yaml:"format"`
-	Caller    bool            `yaml:"caller"`
-	Timestamp TimestampConfig `yaml:"timestamp"`
+	Level     string          `yaml:"level" default:"info"`
+	Target    string          `yaml:"target" default:"stdout"`
+	Format    string          `yaml:"format" default:"json"`
+	Caller    bool            `yaml:"caller" default:"true"`
+	Timestamp TimestampConfig `yaml:"timestamp" mapstructure:"timestamp"`
 }
 
 type TimestampConfig struct {
-	Enabled bool   `yaml:"enabled"`
-	Format  string `yaml:"format"`
+	Enabled bool   `yaml:"enabled" default:"true"`
+	Format  string `yaml:"format" default:"2006-01-0215:04:05.000Z07:00"`
 }
