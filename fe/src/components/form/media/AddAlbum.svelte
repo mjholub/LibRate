25d7bd0b9b9b060eb93f39149f07b394fe952d60
MJH <1a6f1a@riseup.net>
2023-12-16 02:08:45 +0100

    feat: drop area for album cover, genres boxes

--- fe/src/components/form/media/AddAlbum.svelte
+++ fe/src/components/form/media/AddAlbum.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import { PlusIcon } from 'svelte-feather-icons';
+	import { PlusIcon, XIcon } from 'svelte-feather-icons';
 	import type { Album } from '$lib/types/music';
+	import type { Genre } from '$lib/types/media';
 	let album: Album = {
 		UUID: '',
 		kind: 'album',
@@ -27,16 +28,101 @@
 
 	const addMore = () => {};
 	const addImage = () => {};
+	const removeGenre = (index: number) => {
+		if (album.genres) {
+			album.genres.splice(index, 1);
+		}
+	};
+	let genres: string = '';
 </script>
 
-<input type="file" accept="image/*" on:click={addImage} />
-<input bind:value={album.name} />
-<input bind:value={album.album_artists} />
-<!-- render a dropdown allowing to choose whether to add person_artist or group_artist -->
+<div
+	class="drop-area"
+	on:drop={addImage}
+	on:dragover={(e) => e.preventDefault()}
+	aria-dropeffect="copy"
+	role="region"
+	aria-labelledby="drop-area-label"
+>
+	<p id="drop-area-label">Drop or click to add album cover here</p>
+	{#if album.image_paths}
+		<img src={album.image_paths[0]} alt="Album Cover" />
+	{/if}
+</div>
+
+<label for="name">Album Name:</label>
+<input id="name" bind:value={album.name} />
+
+<label for="album-artists">Album Artists:</label>
+<select id="album-artists" bind:value={album.album_artists}>
+	<option value="person_artist">Person</option>
+	<option value="group_artist">Group</option>
+</select>
+
 <button id="add-more" on:click={addMore}>
 	<PlusIcon />
 </button>
-<input bind:value={album.release_date} type="date" />
-<input bind:value={album.genres} />
-<input bind:value={album.duration} type="time" />
+
+<label for="release-date">Release Date:</label>
+<input id="release-date" bind:value={album.release_date} type="date" />
+
+<label for="genres">Genres (comma separated):</label>
+<div>
+	{#if album.genres}
+		{#each album.genres as genre, index}
+			<div class="genre-box">
+				{genre}
+				<span
+					class="remove-genre"
+					on:click={() => removeGenre(index)}
+					on:keyup={(e) => e.key === 'Enter' && removeGenre(index)}
+					aria-label="Remove genre"
+					role="button"
+					tabindex="0"
+				>
+					<XIcon size="12" />
+				</span>
+			</div>
+		{/each}
+	{/if}
+</div>
+<input id="genres" bind:value={genres} on:blur={() => (album.genres = genres.split(','))} />
+
+<label for="duration">Duration:</label>
+<input id="duration" bind:value={album.duration} type="time" />
+
 <p>Tracks:</p>
+<p>Tracks:</p>
+
+<style>
+	.drop-area {
+		border: 2px dashed #ccc;
+		padding: 20px;
+		text-align: center;
+	}
+
+	.drop-area img {
+		max-width: 100%;
+		max-height: 200px;
+		margin-top: 10px;
+	}
+
+	.genre-box {
+		display: inline-block;
+		margin: 0 8px 8px 0;
+		padding: 6px 12px;
+		background-color: #f0f0f0;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		position: relative;
+	}
+
+	.remove-genre {
+		cursor: pointer;
+		position: absolute;
+		top: 50%;
+		right: 8px;
+		transform: translateY(-50%);
+		color: #888;
+	}
+</style>
