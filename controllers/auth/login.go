64958492913891bef8a40fcd6885176dd36cf543
MJH <1a6f1a@riseup.net>
2023-12-18 02:08:29 +0100

    chore(auth): remove unused token implementation

--- controllers/auth/login.go
+++ controllers/auth/login.go
@@ -7,7 +7,6 @@ import (
 	"codeberg.org/mjh/LibRate/models/member"
 
 	"github.com/gofiber/fiber/v2"
-	uuid "github.com/gofrs/uuid/v5"
 
 	h "codeberg.org/mjh/LibRate/internal/handlers"
 )
@@ -85,12 +84,4 @@ func (a *Service) validatePassword(email, login, password string) error {
 	}
 
 	return nil
-}
-
-func (a *Service) createToken() (string, error) {
-	token, err := uuid.NewV7()
-	if err != nil {
-		return "", err
-	}
-	return token.String(), nil
 }
