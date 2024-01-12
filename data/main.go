8e52250048d80242ee0bd9661338a41ea754b2d7
MJH <1a6f1a@riseup.net>
2024-01-12 02:31:27 +0100

    WIP: genre migration query builder

--- data/main.go
+++ data/main.go
@@ -4,60 +4,74 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"regexp"
-	"sort"
 	"strings"
 
-	"github.com/samber/lo"
+	"github.com/goccy/go-json"
 )
 
+type GenreInfo struct {
+	Name        string      `json:"name,omitempty"`
+	Description string      `json:"description,omitempty"`
+	Subgenres   []GenreInfo `json:"children,omitempty"`
+}
+
 func main() {
-	qBytes, err := os.ReadFile(filepath.Join("/", "home", "user", "Projects", "LibRate", "data", "queries2.cypher"))
+	// list all json files in the current directory
+	jsonFiles, err := filepath.Glob("data/*.json")
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("error listing json files: %v", err))
 	}
 
-	lines := strings.Split(string(qBytes), "\n")
-	genresB, err := os.ReadFile(filepath.Join("/", "home", "user", "Projects", "LibRate", "data", "genres.txt"))
-	gen := strings.Split(string(genresB), "\n")
+	var queries []string
+	for _, file := range jsonFiles {
+		file = strings.Split(file, ".")[0]
+		base := strings.Split(file, "/")[1]
+		title := strings.Title(base)
+		query := fmt.Sprintf(`
+		INSERT INTO media.genres (name, kinds) VALUES '%s', ARRAY ['music'] ON CONFLICT DO NOTHING
+		RETURNING id;`, title)
+		queries = append(queries, query)
 
-	re := regexp.MustCompile(`'([A-Za-z0-9,.\-()\"\\' ]*)'`)
-
-	matched := matchLines(lines, gen, re)
-
-	replaced := lo.Associate(gen, func(s string) (string, string) {
-		lowered := strings.ToLower(s)
-		return s, strings.ReplaceAll(lowered, " ", "_")
-	})
-
-	linesToReplace := lo.Keys(matched)
-	sort.Ints(linesToReplace)
-
-	processLines(linesToReplace, matched, replaced, lines)
-
-	data := []byte(strings.Join(lines, "\n"))
-	if err := os.WriteFile("/home/user/Projects/list.txt", data, 0o640); err != nil {
-		panic(fmt.Errorf("error writing to file: %v", err))
-	}
-}
-
-func matchLines(lines, genres []string, re *regexp.Regexp) (lineNumWithSubstr map[int][]string) {
-	lineNumWithSubstr = make(map[int][]string)
-	for i, line := range lines {
-		for _, genre := range genres {
-			if strings.Contains(line, genre) && !re.MatchString(line) && !lo.Contains(lineNumWithSubstr[i], genre) && genre != "" {
-				lineNumWithSubstr[i] = append(lineNumWithSubstr[i], genre)
-			}
+		f, err := os.ReadFile(file + ".json")
+		if err != nil {
+			panic(fmt.Errorf("error reading file %s: %v", file, err))
 		}
-	}
-	return lineNumWithSubstr
-}
+		genres := []GenreInfo{}
+		err = json.Unmarshal(f, &genres)
+		if err != nil {
+			panic(fmt.Errorf("error unmarshalling file %s: %v", file, err))
+		}
+		for _, g := range genres {
+			secondaryGenreQuery := fmt.Sprintf(`INSERT INTO media.genres (name, kinds, parent)
+			VALUES %s, ARRAY ['music'], (SELECT id FROM media.genres WHERE name = '%s')
+			ON CONFLICT DO NOTHING;`, g.Name, title)
 
-func processLines(linesToReplace []int, matched map[int][]string, replaced map[string]string, lines []string) {
-	// defer wg.Done()
-	for i := range linesToReplace {
-		for _, matchingStr := range matched[linesToReplace[i]] {
-			lines[linesToReplace[i]] = strings.ReplaceAll(lines[linesToReplace[i]], string(matchingStr), replaced[string(matchingStr)])
+			descriptionQuery := fmt.Sprintf(`INSERT INTO media.genre_descriptions (language, description, genre_id)
+			VALUES ('en', '%s', (SELECT id FROM media.genres WHERE name = '%s'));`, g.Description, g.Name)
+			queries = append(queries, secondaryGenreQuery)
+			queries = append(queries, descriptionQuery)
+
+			for _, subgenre := range g.Subgenres {
+				query := fmt.Sprintf(
+					`INSERT INTO media.genres (name, kinds, parent)
+				VALUES (%s, ARRAY ['music'], (SELECT id FROM media.genres WHERE name = %s))
+				`, subgenre.Name, g.Name)
+				q2 := fmt.Sprintf(`INSERT INTO media.genre_descriptions (language, description, genre_id)
+				VALUES ('en', '%s', (SELECT id FROM media.genres WHERE name = '%s'));`, subgenre.Description, subgenre.Name)
+				queries = append(queries, query)
+				queries = append(queries, q2)
+			}
+			// then add children to the parent
+			query := fmt.Sprintf(`UPDATE media.genres
+			SET children = ARRAY(SELECT id FROM media.genres 
+			WHERE parent = (SELECT id FROM media.genres WHERE name = '%s')) 
+			WHERE name = '%s';`, title, title)
+			queries = append(queries, query)
+		}
+
+		err = os.WriteFile("queries.sql", []byte(strings.Join(queries, "\n")), 0o644)
+		if err != nil {
+			panic(fmt.Errorf("error writing queries to file: %v", err))
 		}
 	}
 }
