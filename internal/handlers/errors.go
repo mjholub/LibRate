b0cc36ca5114d22ece384893461e0882bb70a6e5
MJH <1a6f1a@riseup.net>
2024-01-13 06:49:32 +0100

    test(handlers): add unit tests for InternalError

--- internal/handlers/errors.go
+++ internal/handlers/errors.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
+)
 
 func Res(c *fiber.Ctx, statusCode int, message string) error {
 	return c.Status(statusCode).JSON(fiber.Map{
@@ -13,4 +16,19 @@ func ResData(c *fiber.Ctx, statusCode int, message string, data interface{}) err
 		"message": message,
 		"data":    data,
 	})
+}
+
+func InternalError(log *zerolog.Logger, c *fiber.Ctx, message string, err error) error {
+	log.Error().Err(err).Msg(message)
+	return Res(c, fiber.StatusInternalServerError, message)
+}
+
+func BadRequest(
+	log *zerolog.Logger,
+	c *fiber.Ctx,
+	clientMessage, serverMessage string,
+	err error,
+) error {
+	log.Error().Msgf("Failed to %s: %v", serverMessage, err)
+	return Res(c, fiber.StatusBadRequest, clientMessage)
 }
