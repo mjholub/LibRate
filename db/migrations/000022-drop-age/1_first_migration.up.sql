6713aaefc46af00a6518339e5941ed1ff39b3be0
MJH <1a6f1a@riseup.net>
2024-01-14 04:23:55 +0100

    fix(db): escape apostrophes in query builder

--- db/migrations/000022-drop-age/1_first_migration.up.sql
+++ db/migrations/000022-drop-age/1_first_migration.up.sql
@@ -11,10 +11,11 @@ CREATE TABLE media.genre_characteristics_mapping (
     genre_id bigint NOT NULL,
     characteristic_id bigint NOT NULL,
     CONSTRAINT genre_characteristics_mapping_pk PRIMARY KEY (genre_id, characteristic_id),
-    CONSTRAINT genre_characteristics_mapping_genre_fk FOREIGN KEY (genre_id) REFERENCES media.genres (id),
-    CONSTRAINT genre_characteristics_mapping_characteristic_fk FOREIGN KEY (characteristic_id) REFERENCES media.genre_characteristics (id)
+    CONSTRAINT genre_characteristics_mapping_genre_fk FOREIGN KEY (genre_id) REFERENCES media.genres (id) ON DELETE CASCADE,
+    CONSTRAINT genre_characteristics_mapping_characteristic_fk FOREIGN KEY (characteristic_id) REFERENCES media.genre_characteristics (id) ON DELETE CASCADE
 );
 
+ALTER TABLE media.genres ADD CONSTRAINT genres_unique UNIQUE ("name");
 ALTER TABLE media.genres DROP COLUMN "characteristics";
 ALTER TABLE media.genres DROP COLUMN description;
 
@@ -23,7 +24,7 @@ CREATE TABLE media.genre_descriptions (
     language varchar(10) NOT NULL,
     description text NULL,
     CONSTRAINT genre_descriptions_pk PRIMARY KEY (genre_id, language),
-    CONSTRAINT genre_descriptions_fk FOREIGN KEY (genre_id) REFERENCES media.genres(id)
+    CONSTRAINT genre_descriptions_fk FOREIGN KEY (genre_id) REFERENCES media.genres(id) ON DELETE CASCADE
 );
 
 ALTER TABLE media.genres ADD kinds _varchar NOT NULL;
