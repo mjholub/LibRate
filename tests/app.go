a0ef3eb62e456223b02bc4550321e9803de1c975
MJH <1a6f1a@riseup.net>
2024-01-14 04:24:18 +0100

    test: universal test fiber app implementation

---
+++ tests/app.go
@@ -0,0 +1,38 @@
+// this file contains the seed code for a mockup test fiber app
+package tests
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/gofiber/contrib/fiberzerolog"
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
+)
+
+func NewAppWithLogger(logger *zerolog.Logger) *fiber.App {
+	fzlog := fiberzerolog.New(fiberzerolog.Config{
+		Logger: logger,
+	})
+
+	app := fiber.New(
+		fiber.Config{
+			DisableStartupMessage: true,
+			ReadTimeout:           60 * time.Second,
+		},
+	)
+	app.Use(fzlog)
+
+	return app
+}
+
+// first check if port 3100 is free, then loop until 65535 until a free port is found
+func TryFindFreePort() (int, error) {
+	for i := 3100; i < 65535; i++ {
+		if _, err := net.Listen("tcp", fmt.Sprintf(":%d", i)); err == nil {
+			return i, nil
+		}
+	}
+	return 0, fmt.Errorf("failed to find a free port for testing")
+}
