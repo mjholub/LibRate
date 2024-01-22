d647084fe69f6fcd9805852f0646ade68c0d9dd2
MJH <1a6f1a@riseup.net>
2024-01-22 11:40:07 +0100

    WIP(fe): film and TV submission mockups

--- fe/src/components/form/media/AddFilm.svelte
+++ fe/src/components/form/media/AddFilm.svelte
@@ -1,14 +1,6 @@
 <script lang="ts">
 	import type { Film } from '$lib/types/film_tv';
-	import {
-		Card,
-		Label,
-		Input,
-		FormGroup,
-		ListGroup,
-		Collapse,
-		ListGroupItem
-	} from '@sveltestrap/sveltestrap';
+	import { Label, Input, Collapse } from '@sveltestrap/sveltestrap';
 
 	let synopsisLanguage: string = 'en';
 
@@ -35,22 +27,50 @@
 	/>
 </svelte:head>
 
-<div class="film-meta">
-	<FormGroup>
-		<Label for="title">Title</Label>
-		<Input type="text" id="title" bind:value={film.title} />
-		<!-- collapsible section for adding cast -->
-		<Collapse>click here to start adding case</Collapse>
-		<Input type="textarea" id="synopsis" bind:value={film.synopsis.String} />
-		<select id="lang" bind:value={synopsisLanguage}>
-			<option value="en">English</option>
-			<option value="fr">French</option>
-		</select>
-		<input type="date" id="releaseDate" bind:value={film.releaseDate.Time} />
-		<input type="number" id="duration" bind:value={film.duration} />
-	</FormGroup>
-</div>
+<form class="sub_form">
+	<p class="warning">Film submission not working yet, this is just a mockup.</p>
+	<Label for="title">Title</Label>
+	<Input type="text" id="title" bind:value={film.title} />
+	<!-- collapsible section for adding cast -->
+	<Collapse
+		>click here to start adding cast
+		<Label for="cast">Cast</Label>
+	</Collapse>
+	<Label for="synopsis">Synopsis</Label>
+	<Input
+		type="textarea"
+		id="synopsis"
+		bind:value={film.synopsis.String}
+		on:input={() => {
+			film.synopsis.Valid = film.synopsis.String.length > 5;
+		}}
+	/>
+	<select id="lang" bind:value={synopsisLanguage}>
+		<option value="en">English</option>
+		<option value="fr">French</option>
+	</select>
+	<Input type="date" id="releaseDate" bind:value={film.releaseDate.Time} />
+	<Label for="duration">Duration</Label>
+	<Input type="number" id="duration" bind:value={film.duration} />
+	<button type="submit">Submit</button>
+</form>
 
 <style>
-	/* Add styles for AddFilm.svelte */
+	.sub_form {
+		display: block;
+	}
+	label {
+		display: block;
+		margin-top: 1rem;
+	}
+	input {
+		margin-top: 0.4rem;
+		margin-bottom: 0.2rem;
+	}
+
+	.warning {
+		color: red;
+		font-weight: bold;
+		font-size: 1.5em;
+	}
 </style>
