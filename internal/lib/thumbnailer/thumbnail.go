2fd53bd1732e6e2838fc329af04dfe35f0d365fa
MJH <1a6f1a@riseup.net>
2024-01-23 10:10:07 +0100

    feat(thumbnailer): respect aspect ratio

--- internal/lib/thumbnailer/thumbnail.go
+++ internal/lib/thumbnailer/thumbnail.go
@@ -14,11 +14,14 @@ import (
 	xdraw "golang.org/x/image/draw"
 )
 
+// Dims denote the maximum width and height of the thumbnail
+// The algorithm will try to resize the image to the maximum possible size that
+// preserves the aspect ratio and fits within the given dimensions.
 type Dims struct {
-	Width, Height uint
+	Width  uint `yaml:"width"`
+	Height uint `yaml:"height"`
 }
 
-// experimental implementation with image/draw, w/o ffmpeg dependency
 func Thumbnail(dims Dims, inputFile string) (thumb image.Image, err error) {
 	f, err := os.Open(inputFile)
 	if err != nil {
@@ -30,7 +33,39 @@ func Thumbnail(dims Dims, inputFile string) (thumb image.Image, err error) {
 	if err != nil {
 		return nil, fmt.Errorf("error decoding image: %v", err)
 	}
-	resized := image.NewRGBA(image.Rect(0, 0, int(dims.Width), int(dims.Height)))
+
+	originalWidth := float64(img.Bounds().Dx())
+	originalHeight := float64(img.Bounds().Dy())
+	aspectRatio := originalWidth / originalHeight
+	// calculate the maximum possible width and height
+	// branching on 1:1 is not worth it as benchmarking shows
+	targeth := uint(float64(dims.Width) / aspectRatio)
+
+	resized := image.NewRGBA(image.Rect(0, 0, int(dims.Width), int(targeth)))
+	xdraw.CatmullRom.Scale(resized, resized.Bounds(), img, img.Bounds(), draw.Over, nil)
+	return resized, nil
+}
+
+func ThumbnailPercentage(percentage float64, inputFile string) (thumb image.Image, err error) {
+	f, err := os.Open(inputFile)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding image: %v", err)
+	}
+	resized := image.NewRGBA(
+		image.Rect(
+			0, 0, int(
+				float64(
+					img.Bounds().Dx())*percentage), int(
+				float64(
+					img.Bounds().Dy())*percentage),
+		),
+	)
 	xdraw.CatmullRom.Scale(resized, resized.Bounds(), img, img.Bounds(), draw.Over, nil)
 	return resized, nil
 }
--- internal/lib/thumbnailer/thumbnail_test.go
+++ internal/lib/thumbnailer/thumbnail_test.go
@@ -9,11 +9,19 @@ import (
 func TestThumbnail(t *testing.T) {
 	dims := Dims{Width: 512, Height: 512}
 
+	// source image 1024x1024
 	thumb, err := Thumbnail(dims, "gopher.jpg")
 	assert.Nil(t, err)
 	bounds := thumb.Bounds()
 	assert.Equal(t, 512, bounds.Dx())
 	assert.Equal(t, 512, bounds.Dy())
+
+	// source image 1600x900-
+	t16x9, err := Thumbnail(dims, "paprikas.png")
+	assert.Nil(t, err)
+	b := t16x9.Bounds()
+	assert.Equal(t, 512, b.Dx())
+	assert.Equal(t, 288, b.Dy())
 }
 
 func BenchmarkThumbnail(b *testing.B) {
