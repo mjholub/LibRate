8b2d0b0dc38ffab2470f5526a6538dc24797eead
MJH <1a6f1a@riseup.net>
2024-01-31 15:37:53 +0100

    SECURITY: harden helmet

--- middleware/security/helmet.go
+++ middleware/security/helmet.go
@@ -11,10 +11,6 @@ import (
 )
 
 func SetupHelmet(conf *cfg.Config) (h fiber.Handler) {
-	fh := conf.Fiber.Host
-	fp := conf.Fiber.Port
-	localAliases := strings.ReplaceAll(fmt.Sprintf(`%s:%d https://%s:%d http://%s:%d https://librate.localhost`,
-		fh, fp, fh, fp, fh, fp), "'", "")
 	return helmet.New(helmet.Config{
 		XSSProtection:             "1; mode=block",
 		ReferrerPolicy:            "no-referrer-when-downgrade",
@@ -22,9 +18,9 @@ func SetupHelmet(conf *cfg.Config) (h fiber.Handler) {
 		CrossOriginEmbedderPolicy: "creadentialless",
 		XFrameOptions:             "ALLOW FROM %s https://www.gravatar.com https://http.cat",
 		ContentSecurityPolicy: fmt.Sprintf(`default-src 'self' https://gnu.org https://www.gravatar.com %s;
-				style-src 'self' cdn.jsdelivr.net 'unsafe-inline';
-				script-src 'self' https://unpkg.com/htmx.org@1.9.9 %s 'unsafe-inline' 'unsafe-eval';
+				style-src 'self' cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css 'unsafe-inline';
+				script-src 'self' %s 'unsafe-inline' 'unsafe-eval';
 				img-src 'self' * %s data: blob:;`,
-			localAliases, localAliases, localAliases),
+			conf.Fiber.Domain, conf.Fiber.Domain, conf.Fiber.Domain),
 	})
 }
