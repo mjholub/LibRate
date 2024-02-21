e3a5523032ae82c3c27b376fd4a33d0b5ce1f684
MJH <1a6f1a@riseup.net>
2024-02-21 13:42:02 +0100

    WIP(search): data cleaning

--- controllers/search/meili/process.go
+++ controllers/search/meili/process.go
@@ -6,13 +6,13 @@ import (
 )
 
 type SearchResponse struct {
-	Categories     []string      `json:"categories"`
-	TotalHits      int64         `json:"totalHits"`
-	ProcessingTime int64         `json:"processingTime"`
-	Page           int64         `json:"page"`
-	TotalPages     int64         `json:"totalPages"`
-	HitsPerPage    int64         `json:"hitsPerPage"`
-	Data           []interface{} `json:"data"`
+	Categories     []string                 `json:"categories"`
+	TotalHits      int64                    `json:"totalHits"`
+	ProcessingTime int64                    `json:"processingTime"`
+	Page           int64                    `json:"page"`
+	TotalPages     int64                    `json:"totalPages"`
+	HitsPerPage    int64                    `json:"hitsPerPage"`
+	Data           []map[string]interface{} `json:"data"`
 }
 
 func processResults(
@@ -33,10 +33,57 @@ func processResults(
 	processed.TotalPages = (processed.TotalHits + hitsPerPage - 1) / hitsPerPage
 	processed.Page = 1
 
+	rawResults := make([]map[string]interface{}, 0)
+
 	for i, category := range processed.Categories {
-		// TODO: remove _rev and _id from the response
-		processed.Data = append(processed.Data, results[category][i].Hits)
+		for _, hit := range results[category][i].Hits {
+			for k, v := range hit.(map[string]interface{}) {
+				results := make(map[string]interface{})
+				results[k] = v
+
+				rawResults = append(rawResults, results)
+			}
+		}
 	}
+	processed.Data = cleanHitsData(rawResults)
 
 	return processed
+}
+
+// FIXME: the first key gets processed properly, but for remaining names (keys)
+// the auxiliary data is incorrectly combined.
+// Example:
+// genreA: (it's proper description)
+// genreB, genreC, genreD: description of genreB
+func cleanHitsData(data []map[string]interface{}) []map[string]interface{} {
+	cleanedData := make([]map[string]interface{}, 0)
+	seen := make(map[string]bool)
+
+	for _, item := range data {
+		if formatted, ok := item["_formatted"].(map[string]interface{}); ok {
+			// Unnest the value of the _formatted key
+			for k, v := range formatted {
+				item[k] = v
+			}
+			delete(item, "_formatted")
+		}
+
+		// Remove _id and _rev keys
+		delete(item, "_id")
+		delete(item, "_rev")
+
+		// Deduplicate
+		id, ok := item["name"].(string)
+		if !ok {
+			continue
+		}
+		if seen[id] {
+			continue
+		}
+		seen[id] = true
+
+		cleanedData = append(cleanedData, item)
+	}
+
+	return cleanedData
 }
